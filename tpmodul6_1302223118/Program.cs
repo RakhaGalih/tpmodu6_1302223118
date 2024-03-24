03a14239d30de0a6b0cfb441c3ccf0b0940ef2b7
rakha <rakhagalih19@gmail.com>
2024-03-24 15:03:58 +0700

    menambahkanclassSayaTubeVideo

---
+++ tpmodul6_1302223118/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract - [RAKHA_GALIH_NUGRAHA_SUKMA]");
+        video.PrintVideoDetails();
+
+        // Menambahkan 100 pemutaran ke video
+        video.IncreasePlayCount(100);
+        video.PrintVideoDetails();
+    }
+}
---
+++ tpmodul6_1302223118/SayaTubeVideo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        this.id = GenerateRandomId();
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("==== video ====");
+        Console.WriteLine($"ID: {id}, \nTitle: {title}, \nPlay Count: {playCount} \n");
+    }
+
+    private int GenerateRandomId()
+    {
+        Random random = new Random();
+        return random.Next(10000, 99999); // Generate 5-digit random ID
+    }
+}
